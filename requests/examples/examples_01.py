4e23190800a4c084e90f1099e214a6a030922822
Bas Dijkstra <bas@ontestautomation.com>
2021-04-12 17:41:02 +0200

    Add example for full body testing in requests

--- requests/examples/examples_01.py
+++ requests/examples/examples_01.py
@@ -32,3 +32,34 @@ def test_get_all_users_check_number_of_users_equals_10():
     response = requests.get("https://jsonplaceholder.typicode.com/users")
     response_body = response.json()
     assert len(response_body) == 10
+
+
+def test_get_user_with_id_1_check_full_body_response():
+
+    expected_response_body = {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    }
+
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    actual_response_body = response.json()
+    assert expected_response_body == actual_response_body
